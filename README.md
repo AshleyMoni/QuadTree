bb3b889915718a7a0893839b9100487e9e305117
Ashley Moni <y30099923@gmail.com>
2014-10-06 11:38:28 -0400

    Set up separate QuadTree generators. Documented them too.

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ QuadTree
 QuadTree library for Haskell, with lens support built in.
 
 Todo:
+
 1) Set up a Quickcheck test suite.
+
 2) Implement traversable instance for the quadtrees?
--- Test/quadtree-tests.hs
+++ Test/quadtree-tests.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import Data.QuadTree.Internal
@@ -7,23 +9,54 @@ import Data.QuadTree.Internal
 import Test.QuickCheck
 import System.Exit (exitSuccess, exitFailure)
 
-import Data.Functor ((<$>))
-import Control.Applicative ((<*>))
 import Control.Lens (set)
+import Control.Monad (replicateM)
+import Data.Functor ((<$>))
 
--- QTs constructed via repetitive setting
-instance (Eq a, Arbitrary a) => Arbitrary (QuadTree a) where
+{- Structure
+
+The QuadTree type has two structural invariants/constraints:
+
+   1. The internal raw tree must not be deeper than its
+   declared depth.
+
+   2. No branch node can have four leaves that are identical.
+   These need to be fused into a single leaf node by the algorithms.
+
+We will acknowledge and manage these invariants by constructing
+two separate Arbitrary generators for QuadTrees:
+
+  1. The first generator will construct QuadTrees strictly using the
+  exposed API (makeTree and setLocation). We'll use this to test if
+  the invariant is consistently maintained across the subset of QuadTrees
+  that the user can construct.
+
+  2. The second generator will generate QuadTrees ex nihilo that obey
+  the invariants. We'll use this for our primary testing purposes, since
+  it can theoretically generate valid non-user-constructable trees and
+  because it can generate large complex trees far far more efficiently.
+
+-}
+
+---- The API-constructable QuadTree generator
+
+newtype APITree a = Constructed (QuadTree a)
+
+instance Show a => Show (APITree a) where
+  show (Constructed qt) = show qt
+
+instance (Eq a, Arbitrary a) => Arbitrary (APITree a) where
   arbitrary = do
-    Positive x <- arbitrary
-    Positive y <- arbitrary
-    base <- arbitrary
-    let baseTree = makeTree (x,y) base
+    Positive len <- arbitrary
+    Positive wid  <- arbitrary
+    baseValue <- arbitrary
+    let baseTree = makeTree (len, wid) baseValue
 
     indices <- listOf $ generateIndex baseTree
     values <- infiniteListOf arbitrary
     let setList = zip indices values
 
-    return $ foldr setAt baseTree setList
+    return . Constructed $ foldr setAt baseTree setList
 
     where setAt (index, value) = set (atLocation index) value
 
@@ -35,21 +68,64 @@ generateIndex qt = do
   return (x,y)
 
 
----- TESTS
+---- Ex-nihilo QuadTree generator
 
-prop_saneTrees :: QuadTree Bool -> Bool
-prop_saneTrees = go <$> treeDepth <*> wrappedTree
+instance (Eq a, Arbitrary a) => Arbitrary (QuadTree a) where
+  arbitrary = do
+    Positive len <- arbitrary
+    Positive wid  <- arbitrary
+    NonNegative depth <- arbitrary
+    tree <- generateNode depth
+
+    return Wrapper { treeLength = len,
+                     treeWidth = wid,
+                     treeDepth = depth,
+                     wrappedTree = tree }
+
+generateNode :: (Eq a, Arbitrary a) => Int -> Gen (Quadrant a)
+generateNode 0 = generateLeaf
+generateNode n = do
+  [a,b,c,d] <- replicateM 4 $ generateEither n
+  return (Node a b c d)
+
+generateLeaf :: Arbitrary a => Gen (Quadrant a)
+generateLeaf = Leaf <$> arbitrary
+
+generateEither :: (Eq a, Arbitrary a) => Int -> Gen (Quadrant a)
+generateEither n = oneof [generateLeaf, generateNode (n - 1)]
+
+
+---- APITree structural tests
+
+-- We use Bools here since they're the most trivial Eq type.
+-- A QuadTree constructed with Bool insertions is the fastest way
+-- to build/fuse up a complex set of nodes at various heights.
+
+-- Inner tree representation cannot be deeper than defined depth
+prop_APITreeDepth :: APITree Bool -> Bool
+prop_APITreeDepth (Constructed qt) = go (treeDepth qt) (wrappedTree qt)
   where go :: Int -> Quadrant a -> Bool
         go _ (Leaf _) = True
         go 0 _        = False
-        go n (Node a b c d) = and [go (n - 1) a,
-                                   go (n - 1) b,
-                                   go (n - 1) c,
-                                   go (n - 1) d]
+        go n (Node a b c d) = and $ fmap (go (n - 1)) [a,b,c,d]
+
+-- Inner tree representation cannot have branches holding four equal leaves
+prop_APITreeInequality :: APITree Bool -> Bool
+prop_APITreeInequality (Constructed qt) = go $ wrappedTree qt
+  where go :: Eq a => Quadrant a -> Bool
+        go (Leaf _)                           = True
+        go (Node (Leaf a) (Leaf b) (Leaf c) (Leaf d))
+          | (a == b) && (b == c) && (c == d)  = False
+        go (Node a b c d) = and $ fmap go [a,b,c,d]
+
+
+---- Ex Nihilo QuadTree tests
+
+
 
 return [] -- Template Haskell splice. See QuickCheck hackage docs.
 runTests :: IO Bool
-runTests = $verboseCheckAll
+runTests = $quickCheckAll
 
 main :: IO ()
 main = do
